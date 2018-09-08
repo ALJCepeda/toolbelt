eb5668231f9626f3c3563b97ec25b3937f33f09b
Alfred Cepeda <AlfredJCepeda@gmail.com>
2018-09-08 01:24:52 -0400

    Some restructuring

---
+++ src/js/util.js
@@ -0,0 +1,62 @@
+export default {
+  // http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  shuffle: function(array) {
+    var currentIndex = array.length;
+    var temporaryValue;
+    var randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex !== 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  },
+  findMissingIndexes: function(array, start, end) {
+    const missing = [];
+    for(let i=start; i<end; i++) {
+      if(array[i] === undefined || array[i] === null) {
+        missing.push(i);
+      }
+    }
+
+    return missing;
+  },
+  rangeFromOffset: function(offset, limit, max = 0) {
+    let start = offset;
+    let end = offset + limit;
+
+    if(limit < 0) {
+      start = end;
+      end = offset;
+
+      if(start < 0) {
+        start = 0;
+        end = end + Math.abs(start);
+      }
+    }
+
+    if(max !== 0 && end > max) {
+      end = max;
+    }
+
+    return { start, end };
+  },
+  once: function(fn, context) {
+		var result;
+		return function() {
+			if(fn) {
+				result = fn.apply(context || this, arguments);
+				fn = null;
+			}
+			return result;
+		};
+	}
+}
--- src/tests/index-test.js
+++ src/tests/index-test.js
@@ -1,5 +1,5 @@
 import redtape from 'redtape';
-import toolbelt from './../js/index.js';
+import util from './../js/util';
 
 const test = redtape();
 
@@ -12,32 +12,32 @@ test('should find all missing indexes', (t) => {
   arr[0] = null;
   arr[5] = null;
 
-  const result = toolbelt.findMissingIndexes(arr, 0, 10);
+  const result = util.findMissingIndexes(arr, 0, 10);
   t.looseEqual(result, [0, 1, 2, 4, 5, 8, 9 ]);
   t.end();
 });
 
 test('Testing rangeFromList', t => t.end());
 test('should create range', (t) => {
-  const result = toolbelt.rangeFromOffset(5, 5);
+  const result = util.rangeFromOffset(5, 5);
   t.looseEqual(result, { start:5, end:10 });
   t.end();
 });
 
 test('should adjust range to max', (t) => {
-  const result = toolbelt.rangeFromOffset(10, 10, 13);
+  const result = util.rangeFromOffset(10, 10, 13);
   t.looseEqual(result, { start:10, end:13 });
   t.end();
 });
 
 test('should accept negatives for limit', (t) => {
-  const result = toolbelt.rangeFromOffset(10, -7);
+  const result = util.rangeFromOffset(10, -7);
   t.looseEqual(result, { start:3, end:10 });
   t.end();
 });
 
 test('should adjust range when negative limit dips below 0', (t) => {
-  const result = toolbelt.rangeFromOffset(4, -7);
+  const result = util.rangeFromOffset(4, -7);
   t.looseEqual(result, { start:0, end:4 });
   t.end();
 });
